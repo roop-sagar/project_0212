733f9e71ab79705b3ee989f7b59b3e9efd1d4a82
Sanjaystorm7119 <101571098+Sanjaystorm7119@users.noreply.github.com>
2022-12-03 22:51:48 +0530

    Revert "Update App.js"
    This reverts commit 8170536740af77adbd51fffe46c2ad70a19df3b0.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Login from './components/Login';
-import Register from './components/Register';
 
 function App() {
   return (
-    <div>
+    <div>hello
     <Login/>
-    <Register/>
     </div>
     
   );
