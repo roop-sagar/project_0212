8170536740af77adbd51fffe46c2ad70a19df3b0
Sanjaystorm7119 <101571098+Sanjaystorm7119@users.noreply.github.com>
2022-12-03 22:43:19 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
-    <div>hello
+    <div>
     <Login/>
+    <Register/>
     </div>
     
   );
