01a60ca8c2db48667e031e853747b91736adc099
Sanjaystorm7119 <101571098+Sanjaystorm7119@users.noreply.github.com>
2022-12-08 23:43:22 +0530

    homePage Background Changed

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
 import "./Home.css"
+import homePage from "../images/homePage.jpg"
 const Home= () => {
     return(
         <div>
             <Navbar/>
-            <img className='bgimage' src='https://images.unsplash.com/photo-1577563908411-5077b6dc7624?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80' alt=""/>
+            <img className='bgimage' src={homePage} alt=""/>
         </div>
     );
 }
